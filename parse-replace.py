d1dcf5685c72eed844d69fbef1d4afc38f327123
PungentArse <64975031+PungentArse@users.noreply.github.com>
2020-06-30 12:29:35 -0600

    Add files via upload

--- parse-replace.py
+++ parse-replace.py
@@ -1,23 +1,22 @@
 import re
 def replace(infile):
+    outFile = open("replaced.c","w")
+    funcPatt = "FUN_"+'[ab-z]?\d+[ab-z]?\d+'
     for line in infile:
         try:
-            outFile = open("replaced.c","a")
-            variables=["local_","local_res","uVar","iVar","puVar","ppHVar","pcVar","lVar","pFVar"]
+            variables=["local_","local_res","uVar","iVar","puVar","ppHVar","pcVar","lVar","pFVar","pHVar"]
             strip = line.strip()
-            
             for index in variables:
                 #print index
                 pattern = index+'[ab-z]?\d+'
-                regexs = re.compile(pattern)
-                if regexs.search(strip):
-                    strip = regexs.sub("repVar",strip)
+                if re.search(pattern,strip):
+                    strip = re.sub(pattern,"repVar",strip)
+                if re.search(funcPatt,strip):
+                    strip = re.sub(funcPatt,"repFunc",strip)
             outFile.write(strip+"\n")
         except IOError:
             print("Error opening file")
-        outFile.close()
-  
-
+    outFile.close()
   
 def fileinput():
     try:
